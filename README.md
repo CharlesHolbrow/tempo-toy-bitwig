7d606f054d161a4bd3b3b69a411798ed2e2f562a
Charles Holbrow <CharlesHolbrow@gmail.com>
2018-10-29 14:45:10 -0400

    add README

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a collection of classes for sending data to `charles-bitwig-osc` BitWig studio extension.
+
+Note the `server` branch, which was used for the my member's week 2018 demo. This serves a basic interface for sending tempo ramps to BitWig.
--- test-project.js
+++ test-project.js
@@ -4,7 +4,7 @@ const project = new bw.Project(160);
 
 const c1 = [48, 50, 53, 55];
 const c2 = [50, 57, 62, 65];
-const b1 =[33, 34, 35, 36];
+const b1 = [33, 34, 35, 36];
 
 // project.newLauncherClip(1, 9, 'Cm', true);
 // project.createRampsStartFirst(c1, 32, b1);
@@ -27,6 +27,3 @@ project.setClipLoop(12*4, 1);
 project.setClipStart(0);
 
 project.close();
-  
-
-
